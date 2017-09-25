02eef934f771050bca73dcf0718787f62c91f3db
0770246 <0770246@KELCE-ACIS15-P>
2017-09-25 16:59:01 -0500

    Assignment backend

--- example1/example1/Page1.xaml.cs
+++ example1/example1/Page1.xaml.cs
@@ -21,5 +21,10 @@ namespace example1
         {
             Navigation.PushAsync(new Page2());
         }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new InteractionPage());
+        }
     }
 }
