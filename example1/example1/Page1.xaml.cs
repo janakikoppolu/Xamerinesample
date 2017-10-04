fe3f18fcbbc009f83bbe04769870b315265b5418
0770246 <0770246@KELCE-ACIS15-P>
2017-10-04 18:28:29 -0500

    final submission

--- example1/example1/Page1.xaml.cs
+++ example1/example1/Page1.xaml.cs
@@ -29,7 +29,7 @@ namespace example1
 
         private void ListButton_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new listpage());
+            Navigation.PushAsync(new Page3());
         }
 
         
---
+++ example1/example1/Page3.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace example1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page3 : ContentPage
+    {
+        public List<string> shoplist { get; set; }
+        public Page3()
+        {
+            InitializeComponent();
+            shoplist = new List<string>();
+        }
+
+        private void additembutton_Clicked(object sender, EventArgs e)
+        {
+            shoplist.Add(additem.Text);
+            shoplistview.ItemsSource = shoplist;
+        }
+
+        private void clrbutton_Clicked(object sender, EventArgs e)
+        {
+            shoplist = new List<string>();
+            shoplistview.ItemsSource = new List<string>();
+        }
+    }
+}
